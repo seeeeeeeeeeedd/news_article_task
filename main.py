5a8771c5e3d08dd6d4d311cca49f36361d15b22b
Sophia <irisyoongi@mail.ru>
2026-08-26 20:26:05 +0700

    добавила демонстрацию работы класса NewsArticle

---
+++ main.py
@@ -0,0 +1,10 @@
+from news_article import NewsArticle
+
+news_article1 = NewsArticle('Первый шаг', 'Анна Сергеева')
+news_article1.print_article()
+
+news_article2 = NewsArticle('', 'Ольга Смирнова')
+news_article2.print_article()
+
+news_article3 = NewsArticle('Дорога домой', 23)
+news_article3.print_article()
