ed68713316dd210d25f252996ba9a50796555aa8
Sophia <irisyoongi@mail.ru>
2026-08-26 19:57:46 +0700

    добавила метод __is_valid_title_and_author, который проверяет корректность данных

--- news_article.py
+++ news_article.py
@@ -2,3 +2,9 @@ class NewsArticle:
     def __init__(self, title: str, author: str):
         self.__title = title
         self.__author = author
+
+    def __is_valid_title_and_author(self, title: str, author: str) -> bool:
+        if isinstance(title, str) and isinstance(author, str):
+            if title.strip() and author.strip():
+                return True
+        return False
