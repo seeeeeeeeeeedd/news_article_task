82b7221838454815ed668d42bd3390b7d3a75848
Sophia <irisyoongi@mail.ru>
2026-09-02 13:50:58 +0700

    добавила в NewsPortal список опубликованных статей

--- news_portal.py
+++ news_portal.py
@@ -2,6 +2,9 @@ from news_article import NewsArticle
 
 
 class NewsPortal:
+    def __init__(self):
+        self.__published_articles = []
+
     def publish_article(self, article):
         is_valid = self.__is_valid_article(article)
 
@@ -9,12 +12,12 @@ class NewsPortal:
             current_title = article.get_title()
             current_author = article.get_author()
             print(f'Статья "{current_title}", опубликована автором {current_author}')
+            self.__published_articles.append(article)
         else:
             print('Ошибка. Некорректные данные')
             return
 
     def __is_valid_article(self, article):
-
         if not isinstance(article, NewsArticle):
             return False
         else:
