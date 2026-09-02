dfa7efbcc3ab9390abd32d4933c88a3534475758
Sophia <irisyoongi@mail.ru>
2026-09-02 13:22:07 +0700

    добавила геттеры get_title и get_author

--- news_article.py
+++ news_article.py
@@ -8,6 +8,12 @@ class NewsArticle:
             self.__title = 'Без названия'
             self.__author = 'Неизвестный автор'
 
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
     def __is_valid_title_and_author(self, title: str, author: str) -> bool:
         if isinstance(title, str) and isinstance(author, str):
             if title.strip() and author.strip():
