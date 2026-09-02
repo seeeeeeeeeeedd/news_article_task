0183af65b991beccf8ad44c42d3f80359d6d1983
Sophia <irisyoongi@mail.ru>
2026-09-02 13:32:00 +0700

    создала класс NewsPortal и два метода - publish_article и __is_valid_article, которые отвечают за проверку корректности данных и их публикацию

---
+++ news_portal.py
@@ -0,0 +1,19 @@
+from news_article import NewsArticle
+
+
+class NewsPortal:
+    def publish_article(self, article):
+        is_valid = self.__is_valid_article(article)
+
+        if is_valid:
+            current_title = article.get_title()
+            current_author = article.get_author()
+            print(f'Статья "{current_title}", опубликована автором {current_author}')
+        else:
+            print('Ошибка. Некорректные данные')
+            return
+
+    def __is_valid_article(self, article):
+
+        if not isinstance(article, NewsArticle):
+            return False
