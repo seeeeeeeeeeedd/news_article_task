c03de59b3aff736dbefbfaa41f17a551d89b58a5
Sophia <irisyoongi@mail.ru>
2026-09-02 13:42:33 +0700

    обновила демонстрацию работы классов -  публикация через NewsPortal

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from news_article import NewsArticle
+from news_portal import NewsPortal
+
+news_portal = NewsPortal()
 
 news_article1 = NewsArticle('Первый шаг', 'Анна Сергеева')
-news_article1.print_article()
+news_portal.publish_article(news_article1)
 
 news_article2 = NewsArticle('', 'Ольга Смирнова')
-news_article2.print_article()
+news_portal.publish_article(news_article2)
 
 news_article3 = NewsArticle('Дорога домой', 23)
-news_article3.print_article()
+news_portal.publish_article(news_article3)
