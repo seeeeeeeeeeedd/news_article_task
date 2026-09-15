727933b016e553e158779e42133504ccda46239c
Sophia <irisyoongi@mail.ru>
2026-09-15 15:32:48 +0700

    создала класс Author с проверкой имени и геттером для передачи имени автора при создании статьи

---
+++ author.py
@@ -0,0 +1,17 @@
+class Author:
+    def __init__(self, name: str):
+        is_valid = self.__is_valid_name(name)
+
+        if is_valid:
+            self.__name = name
+        else:
+            self.__name = 'Неизвестный автор'
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def __is_valid_name(self, name: str) -> bool:
+        if isinstance(name, str):
+            if name.strip():
+                return True
+        return False
