407081610e6b3e3a92c184c2eb489915882747da
Sophia <irisyoongi@mail.ru>
2026-09-15 15:16:18 +0700

    отделила блоки if пустой строкой

--- news_article.py
+++ news_article.py
@@ -1,6 +1,7 @@
 class NewsArticle:
     def __init__(self, title: str, author: str):
         is_valid = self.__is_valid_title_and_author(title, author)
+
         if is_valid:
             self.__title = title
             self.__author = author
@@ -15,6 +16,7 @@ class NewsArticle:
         return self.__author
 
     def __is_valid_title_and_author(self, title: str, author: str) -> bool:
+
         if isinstance(title, str) and isinstance(author, str):
             if title.strip() and author.strip():
                 return True
