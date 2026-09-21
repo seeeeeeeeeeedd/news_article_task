23bb21dbdb8c7200ecb89a1a802334b587f6a27b
Sophia <irisyoongi@mail.ru>
2026-09-21 16:08:56 +0700

    добавила поле content и геттер get_content для текста содержания статьи

--- news_article.py
+++ news_article.py
@@ -2,18 +2,23 @@ from author import Author
 
 
 class NewsArticle:
-    def __init__(self, title: str, author: Author):
+    def __init__(self, title: str, content: str, author: Author):
         is_valid = self.__is_valid_title_and_author(title, author)
 
         if is_valid:
             self.__title = title.capitalize()
+            self.__content = content
             self.__author = author
         else:
             self.__title = 'Без названия'
-            self.__author = 'Неизвестный автор'
+            self.__content = 'Содержание отсутствует'
+            self.__author = Author('Неизвестный автор')
 
     def get_title(self):
         return self.__title
+
+    def get_content(self):
+        return self.__content
 
     def get_author(self):
         return self.__author
